f16cb059dba0e870d9c2ac7fc7234edcfa56b021
Ian Thompson <ianthompson@nicelion.com>
2016-11-06 18:57:06 -0500

    Add files via upload

--- snowy_day.py
+++ snowy_day.py
@@ -28,7 +28,7 @@ pygame.display.set_caption(TITLE)
 
 # Timer
 clock = pygame.time.Clock()
-refresh_rate = 30
+refresh_rate = 60
 
 
 def draw_cloud(x, y):
@@ -53,8 +53,9 @@ def draw_bush(x, y):
 
     pygame.draw.rect(screen, PYColor.greenColor(), [x + 20, y + 20, 65, 40])
 
-def draw_light(x,y):
-    pygame.draw.ellipse(screen, PYColor.randomLightColor(), [x, y, 10, 10])
+def draw_light(x,y,r):
+
+    pygame.draw.ellipse(screen, PYColor.randomLightColor(), [x, y, r, r])
 
 
 
@@ -78,7 +79,31 @@ night = False
 
 def santa():
 
-    pygame.draw.rect(screen, PYColor.redColor(), [636, 80, 100, 50])
+    pygame.draw.line(screen, PYColor.brownColor(), [707, 120],[691, 104],3)
+    pygame.draw.line(screen, PYColor.brownColor(), [670, 105],[655, 119],3)
+    pygame.draw.polygon(screen, PYColor.colorWithRGB(155, 12, 0), [[683,72], [671,80], [691, 81]])
+    pygame.draw.ellipse(screen, PYColor.colorWithRGB(247, 217, 158), [675, 55, 20, 20])
+    pygame.draw.ellipse(screen, PYColor.blackColor(), [679, 62, 5, 5])
+    pygame.draw.polygon(screen, PYColor.colorWithRGB(155, 12, 0), [[684, 40],[673, 57],[697, 57]])
+    pygame.draw.ellipse(screen, PYColor.whiteColor(), [682, 37, 7, 7])
+    pygame.draw.line(screen, PYColor.whiteColor(), [672, 56],[695, 56],3)
+    pygame.draw.rect(screen, PYColor.redColor(), [636, 80, 90, 30])
+    pygame.draw.line(screen, PYColor.brownColor(), [620,120],[737,120],3)
+    pygame.draw.line(screen, PYColor.colorWithRGB(155, 12, 0), [669, 81],[667, 73], 5)
+
+    # Reigndeer
+
+    pygame.draw.ellipse(screen, PYColor.brownColor(), [500, 78, 20, 20])
+    pygame.draw.line(screen, PYColor.brownColor(), [514, 89],[541, 103], 10)
+    pygame.draw.line(screen, PYColor.brownColor(), [545, 108],[537, 125], 5)
+    pygame.draw.line(screen, PYColor.brownColor(), [567, 112],[577, 125], 5)
+
+    pygame.draw.rect(screen, PYColor.brownColor(), [531, 95, 50, 20])
+    pygame.draw.ellipse(screen, PYColor.redColor(), [498, 87, 7, 7])
+    pygame.draw.ellipse(screen, PYColor.blackColor(), [507, 83, 5, 5])
+
+    pygame.draw.line(screen, PYColor.colorWithRGB(109, 51, 0), [552, 99],[665, 73], 5)
+
 
 while not done:
     # Event processing
@@ -143,11 +168,14 @@ while not done:
         pygame.draw.line(screen, PYColor.blackColor(), [0, 410- 3], [Screen.x, 410-3], 3)
         pygame.draw.line(screen, PYColor.brownColor(), [0, 410], [Screen.x, 410], 5)
 
+    santa()
 
     # screen.blit(img,(0,0))
 
 
     # House
+
+    pygame.draw.rect(screen, PYColor.colorWithRGB(96, 35, 7), [300, 150, 50, 100])
 
     pygame.draw.polygon(screen, PYColor.houseColor(), [[204, 66+50], [30, 200+50], [390, 200+50]])
     pygame.draw.polygon(screen, PYColor.blackColor(), [[204, 66+50], [30, 200+50], [390, 200+50]],3)
@@ -156,36 +184,51 @@ while not done:
 
     pygame.draw.rect(screen, PYColor.houseColor(), [60, 250, 300, 250])
     pygame.draw.rect(screen, PYColor.blackColor(), [60, 250, 300, 250], 3)
+    '''Door'''
+    pygame.draw.rect(screen, PYColor.darkBownColor(), [165, 370, 90,130])
+    pygame.draw.rect(screen, PYColor.blackColor(), [165, 370, 90,130], 3)
 
-    pygame.draw.rect(screen, PYColor.darkBownColor(), [160, 370, 90,130])
-    pygame.draw.rect(screen, PYColor.blackColor(), [160, 370, 90,130], 3)
-
-    pygame.draw.rect(screen, PYColor.blueColor(), [260, 360, 90, 110])
+    pygame.draw.rect(screen, PYColor.yellowColor(), [260, 360, 90, 110])
+    pygame.draw.rect(screen, PYColor.yellowColor(), [70, 360, 90, 110])
 
     pygame.draw.polygon(screen, PYColor.greenColor(), [[349, 352+30], [349, 379+30], [334, 379+30]])
     pygame.draw.polygon(screen, PYColor.greenColor(), [[349, 369+30], [349, 399+30], [324, 399+30]])
     pygame.draw.polygon(screen, PYColor.greenColor(), [[349, 429-10], [349, 444], [315, 444]])
     pygame.draw.polygon(screen, PYColor.greenColor(), [[349, 429-5], [349, 444+25], [310, 444+25]])
 
+    draw_light(343,397, 5)
+    draw_light(331, 424, 5)
+    draw_light(341, 421, 5)
+    draw_light(341,431, 5)
+    draw_light(326,441, 5)
+    draw_light(344,449, 5)
+    draw_light(334, 446, 5)
+    draw_light(339, 407, 5)
+
+
+
     pygame.draw.rect(screen, PYColor.blackColor(), [305, 360, 5, 110])
     pygame.draw.line(screen, PYColor.blackColor(), [260, 412], [350, 412], 5)
+    pygame.draw.rect(screen, PYColor.blackColor(), [113, 360, 5, 110])
+    pygame.draw.line(screen, PYColor.blackColor(), [70, 412], [157, 412], 5)
 
     pygame.draw.rect(screen, PYColor.blackColor(), [260, 360, 90, 110],3)
+    pygame.draw.rect(screen, PYColor.blackColor(), [70, 360, 90, 110],3)
 
     draw_bush(60, 450)
     draw_bush(280, 450)
-    draw_light(89,475)
-    draw_light(113,466)
-    draw_light(137,482)
-    draw_light(74,493)
-    draw_light(110,496)
-    draw_light(308,473)
-    draw_light(296,489)
-    draw_light(322,496)
-    draw_light(360,494)
-    draw_light(341,458)
-    draw_light(319, 460)
-    draw_light(341, 479)
+    draw_light(89,475,10)
+    draw_light(113,466,10)
+    draw_light(137,482,10)
+    draw_light(74,493,10)
+    draw_light(110,496,10)
+    draw_light(308,473,10)
+    draw_light(296,489,10)
+    draw_light(322,496,10)
+    draw_light(360,494,10)
+    draw_light(341,458,10)
+    draw_light(319, 460,10)
+    draw_light(341, 479,10)
 
 
 
@@ -199,20 +242,20 @@ while not done:
     pygame.draw.polygon(screen, PYColor.orangeColor(), [[800, 340], [830, 330], [830, 340]])
 
 
-    pygame.draw.ellipse(screen, PYColor.blackColor(), [810 ,320, 10 , 10])
-    pygame.draw.ellipse(screen, PYColor.blackColor(), [830 ,320, 10 , 10])
+    pygame.draw.ellipse(screen, PYColor.blackColor(), [810,320, 10, 10])
+    pygame.draw.ellipse(screen, PYColor.blackColor(), [830,320, 10, 10])
 
 
     '''Mouth'''
-    pygame.draw.ellipse(screen, PYColor.blackColor(), [800 ,340, 7 , 7])
-    pygame.draw.ellipse(screen, PYColor.blackColor(), [810 ,350, 7 , 7])
-    pygame.draw.ellipse(screen, PYColor.blackColor(), [820 ,352, 7 , 7])
+    pygame.draw.ellipse(screen, PYColor.blackColor(), [800,340, 7, 7])
+    pygame.draw.ellipse(screen, PYColor.blackColor(), [810,350, 7, 7])
+    pygame.draw.ellipse(screen, PYColor.blackColor(), [820,352, 7, 7])
 
 
-    pygame.draw.ellipse(screen, PYColor.whiteColor(), [780, 360, 90 , 90])
-    pygame.draw.ellipse(screen, PYColor.blackColor(), [780, 360, 90 , 90], 1)
-    pygame.draw.ellipse(screen, PYColor.whiteColor(), [775, 400, 100 , 100])
-    pygame.draw.ellipse(screen, PYColor.blackColor(), [775, 400, 100 , 100], 1)
+    pygame.draw.ellipse(screen, PYColor.whiteColor(), [780, 360, 90, 90])
+    pygame.draw.ellipse(screen, PYColor.blackColor(), [780, 360, 90, 90], 1)
+    pygame.draw.ellipse(screen, PYColor.whiteColor(), [775, 400, 100, 100])
+    pygame.draw.ellipse(screen, PYColor.blackColor(), [775, 400, 100, 100], 1)
 
 
 # Update screen
