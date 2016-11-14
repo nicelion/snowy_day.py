f0ac98c8874045173aebbeea494ed502e32145b9
Ian Thompson <ianthompson@nicelion.com>
2016-11-14 07:20:47 -0500

    Add files via upload

--- snowy_day.py
+++ snowy_day.py
@@ -1,52 +1,21 @@
-
-# Computer Programming 1
+## Computer Programming 1
 # Unit 11 - Graphics
 #
 # A scene that uses loops to make stars and make a picket fence.
+#
+# Ian Thompson 256,97
 # Imports
 import pygame
 import random
 import PYColor
 import math
-import candy_cane_points
-from pygame.locals import*
-import os
-
-# img = pygame.image.load('/Users/ianthompson/Desktop/snowy_day/santassleigh.png')
 
 class Screen:
-    x = 1000
-    y = 600
+    x = 1000  # screen width
+    y = 600   # screen height
 
 class lightColor:
-
-
     colors = [PYColor.colorWithRGB(34, 204, 0), PYColor.rgb(225, 239, 31), PYColor.redColor()]
-
-
-
-
-
-
-    #
-    # for color in colors:
-    #     if color[0] == color[]
-
-    # list(set(colors))
-    # a = []
-    # for x in colors:
-    #     if x not in a:
-    #         c_1 = colors[0]
-    #         c_2 = colors[1]
-    #         c_3 = colors[2]
-    #         a.append(x)
-    #         print("if not called")
-    #     else:
-    #         colors[0] = PYColor.randomLightColor()
-    #         colors[1] = PYColor.randomLightColor()
-    #         colors[2] = PYColor.randomLightColor()
-    #         print("else called")
-
 
 # Initialize game engine
 pygame.init()
@@ -61,7 +30,33 @@ pygame.display.set_caption(TITLE)
 
 # Timer
 clock = pygame.time.Clock()
-refresh_rate = 30
+refresh_rate = 60
+
+x1 = 300
+santas_position = [635, 20]
+santa_x = 635
+nose_length = 800
+
+lights_on = True
+key_down_left = False
+key_down_right = False
+key_down_up = False
+key_down_down = False
+key_down_equals = False
+key_down_minus = False
+
+santas_sleigh = pygame.Surface([256, 100], pygame.SRCALPHA, 32)
+mask=pygame.Surface((256,100),pygame.SRCALPHA)
+
+
+
+presents = []
+
+done = False
+night = False
+arms_up = True
+ticks = 0
+
 
 
 def draw_cloud(x, y):
@@ -71,13 +66,16 @@ def draw_cloud(x, y):
     pygame.draw.ellipse(screen, PYColor.cloudColor(), [x + 35, y, 50, 50])
     pygame.draw.rect(screen, PYColor.cloudColor(), [x + 20, y + 20, 60, 40])
 
-def draw_smoke(x, y):
-    pygame.draw.ellipse(screen, PYColor.whiteColor(), [x, y, 40, 60])
-    pygame.draw.ellipse(screen, PYColor.whiteColor(), [x, y, 60, 40])
-    pygame.draw.ellipse(screen, PYColor.whiteColor(), [x, y, 50, 50])
+def draw_smoke():
+    for s in smoke:
+        x = s
+        y = (17 *(0-1) * (math.sqrt(x)) + 450) - 15
+        # y = (math.sqrt(x) * 50) + 450
+        pygame.draw.ellipse(screen, PYColor.whiteColor(), [x, y, 25, 25])
+        pygame.draw.ellipse(screen, PYColor.whiteColor(), [x - 5, y, 30, 20])
 
-
-
+    if s == 850:
+        x = s[2]
 
 def draw_bush(x, y):
     pygame.draw.ellipse(screen, PYColor.greenColor(), [x, y + 20, 40, 40])
@@ -107,11 +105,11 @@ for i in range(25):
     y = random.randrange(0,200)
     clouds.append([x, y])
 
-smoke=[]
-for i in range(2):
-    x = random.randrange(0, 600)
-    y = random.randrange(0, 150)
-    smoke.append([x,y])
+smoke = []
+for s in range(20):
+    x = s * 50 + 310
+    smoke.append(x)
+
 snow = []
 for n in range(1000):
     x = random.randrange(0, Screen.x)
@@ -120,56 +118,170 @@ for n in range(1000):
     speed = random.randrange(1,4)
     snow.append([x, y, r, r])
 
-# Game loop
-done = False
-night = False
+present = []
+for n in range(1):
+    x = santas_position[0]
+    y = santas_position[1]
+    present.append([x, y])
 
-def santa():
+last_direction_left = True
+def santa(x,y, frame, trans):
 
-    pygame.draw.line(screen, PYColor.brownColor(), [707, 120],[691, 104],3)
-    pygame.draw.line(screen, PYColor.brownColor(), [670, 105],[655, 119],3)
-    pygame.draw.polygon(screen, PYColor.colorWithRGB(155, 12, 0), [[683,72], [671,80], [691, 81]])
-    pygame.draw.ellipse(screen, PYColor.colorWithRGB(247, 217, 158), [675, 55, 20, 20])
-    pygame.draw.ellipse(screen, PYColor.blackColor(), [679, 62, 5, 5])
-    pygame.draw.polygon(screen, PYColor.colorWithRGB(155, 12, 0), [[684, 40],[673, 57],[697, 57]])
-    pygame.draw.ellipse(screen, PYColor.whiteColor(), [682, 37, 7, 7])
-    pygame.draw.line(screen, PYColor.whiteColor(), [672, 56],[695, 56],3)
-    pygame.draw.rect(screen, PYColor.redColor(), [636, 80, 90, 30])
-    pygame.draw.line(screen, PYColor.brownColor(), [620,120],[737,120],3)
-    pygame.draw.line(screen, PYColor.colorWithRGB(155, 12, 0), [669, 81],[667, 73], 5)
 
-    # Reigndeer
+    red = (206, 41, 26, trans)
+    dark_red = (86, 10, 0, trans)
 
-    pygame.draw.ellipse(screen, PYColor.brownColor(), [500, 78, 20, 20])
-    pygame.draw.line(screen, PYColor.brownColor(), [514, 89],[541, 103], 10)
-    pygame.draw.line(screen, PYColor.brownColor(), [545, 108],[537, 125], 5)
-    pygame.draw.line(screen, PYColor.brownColor(), [567, 112],[577, 125], 5)
+    if frame == 0:
+        red = (206, 41, 26, trans)
+    elif frame == 1:
+        red = dark_red
 
-    pygame.draw.rect(screen, PYColor.brownColor(), [531, 95, 50, 20])
-    pygame.draw.ellipse(screen, PYColor.redColor(), [498, 87, 7, 7])
-    pygame.draw.ellipse(screen, PYColor.blackColor(), [507, 83, 5, 5])
 
-    pygame.draw.line(screen, PYColor.colorWithRGB(109, 51, 0), [552, 99],[665, 73], 5)
+    if key_down_left or last_direction_left:
 
-def test(x):
+        pygame.draw.ellipse(santas_sleigh, (132, 11, 0), [190, 21, 45, 35])  # sack of toys
+        pygame.draw.polygon(santas_sleigh, (132, 11, 0, trans), [[171, 33], [162, 45], [177, 45]]) # body
+        pygame.draw.line(santas_sleigh, (132, 11, 0, trans), [162, 44], [156, 33], 3)  # arm
 
-    pygame.draw.ellipse(screen, PYColor.orangeColor(), [x + 526, 176, 100, 100])
+        pygame.draw.ellipse(santas_sleigh, (255, 219, 183, trans), [162, 15, 20, 20])  # santa's head
+        pygame.draw.ellipse(santas_sleigh, (0,0,0, trans), [163, 22, 5, 5])  # santa's head
+        pygame.draw.polygon(santas_sleigh, (237, 27, 0, trans), [[159, 18], [183, 18], [170, 5]])  # hat
+        pygame.draw.ellipse(santas_sleigh, (255,255,255, trans), [169, 4, 5, 5]) # hat ball
 
-lights_on = True
+        pygame.draw.line(santas_sleigh, (255,255,255, trans), [159, 18], [183, 18], 3)
+
+        pygame.draw.line(santas_sleigh, (153, 98, 16, trans), [180, 75], [163, 94], 3)  # ski support
+        pygame.draw.line(santas_sleigh, (153, 98, 16, trans), [206, 71], [227, 93], 3)  # ski support
+
+        pygame.draw.rect(santas_sleigh, (206, 41, 26, trans), [148, 40, 90, 40])  # sleigh
+        pygame.draw.line(santas_sleigh, (153, 98, 16, trans), [251, 94], [127, 94], 3)  # ski
+
+        # reindeer
+
+        pygame.draw.line(santas_sleigh, (112, 31, 0, trans), [20, 43], [24, 28], 3)  # antler
+        pygame.draw.line(santas_sleigh, (112, 31, 0, trans), [24, 42], [30, 33], 3)  # antler
+        pygame.draw.ellipse(santas_sleigh, (153, 98, 16, trans), [10, 40, 25, 25])  # head
+        pygame.draw.rect(santas_sleigh, (153, 98, 16, trans), [35, 57, 70, 25])  # body
+        pygame.draw.line(santas_sleigh, (153, 98, 16, trans), [40, 67], [22, 49], 13)  # neck
+        pygame.draw.ellipse(santas_sleigh, red, [5, 50, 10, 10])  # nose
+        pygame.draw.ellipse(santas_sleigh, (0,0,0, trans), [18, 45, 7, 7])  # nose
+        pygame.draw.line(santas_sleigh, (112, 31, 0, trans), [69, 65], [156, 33], 3)  # leash
+
+        pygame.draw.line(santas_sleigh, (153, 98, 16, trans), [45, 75], [39, 88], 3)  # leg
+        pygame.draw.line(santas_sleigh, (153, 98, 16, trans), [55, 76], [47, 91], 3)  # leg
+        pygame.draw.line(santas_sleigh, (153, 98, 16, trans), [82, 72], [95, 89], 3)  # leg
+        pygame.draw.line(santas_sleigh, (153, 98, 16, trans), [80, 79], [88, 88], 3)  # leg
+
+        screen.blit(santas_sleigh, (x, y))
+
+        flipped = False
+
+
+
+    if key_down_right or not last_direction_left:
+
+        other2 = pygame.transform.flip(santas_sleigh, True, False)
+        screen.blit(other2, (x, y))
+    # if key_down_down and key_down_left:
+    #
+    #     other3 = pygame.transform.rotate(santas_sleigh, 5)
+    #     screen.blit(other3, (x, y))
+
+
+
+
+
+def draw_present(x,y):
+    pygame.draw.rect(screen, PYColor.redColor(), [x, y + 15, 65, 50])
+    pygame.draw.line(screen, PYColor.colorWithRGB(255,255,0), [x + 0, y + 41], [x + 63, y + 41], 10)  # ribbon
+    pygame.draw.line(screen, PYColor.colorWithRGB(255,255,0), [x + 38, y + 14], [x + 38, y + 65], 10)  # ribbon
+    pygame.draw.ellipse(screen, PYColor.colorWithRGB(255,255,0), [x + 30, y + 3, 12, 12])
+    pygame.draw.polygon(screen, PYColor.colorWithRGB(255,255,0), [[x + 39, y + 8], [x + 53, y + 0], [x + 53, y + 11]])
+    pygame.draw.polygon(screen, PYColor.colorWithRGB(255,255,0), [[x + 31, y + 8], [x + 16, y + 0], [x + 17, y + 12]])
+
+# Game Loop
 
 while not done:
+
+    frame = ticks // 10
+
+    ticks += 1
+
+    if ticks >= 60:
+        ticks = 0
+
     # Event processing
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
                 done = True
-        elif event.type == pygame.MOUSEBUTTONUP:  # or MOUSEBUTTONDOWN depending on what you want.
+        elif event.type == pygame.MOUSEBUTTONUP:
             print(event.pos)
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 lights_on = not lights_on
+            if event.key == pygame.K_LEFT:
+                key_down_left = True  # sets key_down bool to true
+            if event.key == pygame.K_RIGHT:
+                key_down_right = True
+            if event.key == pygame.K_v:
+                print(santas_position[0], santas_position[1])
+            if event.key == pygame.K_a:
+                arms_up = not arms_up
+            if event.key == pygame.K_UP:
+                key_down_up = True
+            if event.key == pygame.K_DOWN:
+                key_down_down = True
+            if event.key == pygame.K_MINUS:
+                key_down_minus = True
+            if event.key == pygame.K_EQUALS:
+                key_down_equals = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:  # left key has been un-clicked
+                key_down_left = False
+            if event.key == pygame.K_RIGHT:  # right key has been un-clicked
+                key_down_right = False
+            if event.key == pygame.K_UP:
+                key_down_up = False
+            if event.key == pygame.K_DOWN:
+                key_down_down = False
+            if event.key == pygame.K_MINUS:
+                key_down_minus = False
+            if event.key == pygame.K_EQUALS:
+                key_down_equals = False
 
-
-
+    if key_down_left:
+        if santas_position[0] == -265:
+            santas_position[0] = Screen.x + 10
+        else:
+            santas_position[0] -= 5
+            last_direction_left = True
+    if key_down_right:
+        if santas_position[0] == 1100:
+            santas_position[0] = -265
+        else:
+            santas_position[0] += 5
+            last_direction_left = False
+    if key_down_up:
+        if santas_position[1] <= -10:
+            santas_position[1] = santas_position[1]
+        else:
+            santas_position[1] -= 5
+    if key_down_down:
+        if santas_position[1] >= 290:
+            santas_position[1] = santas_position[1]
+        else:
+            santas_position[1] += 5
+    if key_down_minus:
+        if nose_length >= 800:
+            nose_length = nose_length
+        else:
+            nose_length += 5
+    elif key_down_equals:
+        if nose_length <= 365:
+            nose_length = nose_length
+        else:
+            nose_length -= 5
 
     # Game logic
     ''' move clouds '''
@@ -184,29 +296,43 @@ while not done:
         if s[1] > 700:
             s[0] = random.randrange(0, Screen.x)
             s[1] = random.randrange(-200, 0)
+    for p in present:
+        p[1] += 2
 
+        if p[1] > 354:
+            p[0] = santas_position[0]
+            p[1] = santas_position[1]
     # Drawing code
 
-    ''' sky '''
+    # sky
     screen.fill(PYColor.blackColor())
+
+
 
     for s in snow:
         pygame.draw.ellipse(screen, PYColor.whiteColor(), s)
 
-    ''' sun '''
+    # for p in present:
+    #     draw_present(p[0] + 150, p[1])
 
+    # sun
     pygame.draw.ellipse(screen, PYColor.whiteColor(), [775, 75, 100, 100])
 
-    ''' clouds '''
+    # clouds
     for c in clouds:
         draw_cloud(c[0], c[1])
 
 
-    ''' grass '''
+    smoke = [s + 1 if s < 800 else 310 for s in smoke]
+
+
+    draw_smoke()
+
+    # grass
     pygame.draw.rect(screen, PYColor.whiteColor(), [0, 400, Screen.x, 200])
 
 
-    ''' fence '''
+    # fence
     y = 380
     for x in range(5, Screen.x, 30):
         pygame.draw.polygon(screen, PYColor.brownColor(), [[x+5, y], [x+10, y+5],
@@ -224,10 +350,9 @@ while not done:
         pygame.draw.line(screen, PYColor.blackColor(), [0, 410- 3], [Screen.x, 410-3], 3)
         pygame.draw.line(screen, PYColor.brownColor(), [0, 410], [Screen.x, 410], 5)
 
-    santa()
 
-    # screen.blit(img,(0,0))
 
+    santa(santas_position[0], santas_position[1], frame, 255)
 
     # House
 
@@ -241,7 +366,7 @@ while not done:
     pygame.draw.rect(screen, PYColor.houseColor(), [60, 250, 300, 250])
     pygame.draw.rect(screen, PYColor.blackColor(), [60, 250, 300, 250], 3)
 
-    '''Door'''
+    # door
     pygame.draw.rect(screen, PYColor.darkBownColor(), [165, 370, 90,130])
     pygame.draw.rect(screen, PYColor.blackColor(), [165, 370, 90,130], 3)
 
@@ -253,7 +378,7 @@ while not done:
     else:
         pygame.draw.rect(screen, PYColor.blueColor(), [260, 360, 90, 110])
         pygame.draw.rect(screen, PYColor.blueColor(), [70, 360, 90, 110])
-        
+
     pygame.draw.polygon(screen, PYColor.greenColor(), [[349, 352+30], [349, 379+30], [334, 379+30]])
     pygame.draw.polygon(screen, PYColor.greenColor(), [[349, 369+30], [349, 399+30], [324, 399+30]])
     pygame.draw.polygon(screen, PYColor.greenColor(), [[349, 429-10], [349, 444], [315, 444]])
@@ -300,22 +425,27 @@ while not done:
     pygame.draw.ellipse(screen, PYColor.whiteColor(), [785, 300, 80 , 80])
     pygame.draw.ellipse(screen, PYColor.blackColor(), [785, 300, 80 , 80], 1)
 
-
-    '''Nose'''
-    pygame.draw.polygon(screen, PYColor.orangeColor(), [[800, 340], [830, 330], [830, 340]])
+    # nose
+    pygame.draw.polygon(screen, PYColor.orangeColor(), [[nose_length, 340], [830, 330], [830, 340]])
 
 
     pygame.draw.ellipse(screen, PYColor.blackColor(), [810,320, 10, 10])
     pygame.draw.ellipse(screen, PYColor.blackColor(), [830,320, 10, 10])
 
 
-    '''Mouth'''
+    # mouth
     pygame.draw.ellipse(screen, PYColor.blackColor(), [800,340, 7, 7])
     pygame.draw.ellipse(screen, PYColor.blackColor(), [810,350, 7, 7])
     pygame.draw.ellipse(screen, PYColor.blackColor(), [820,352, 7, 7])
+    pygame.draw.ellipse(screen, PYColor.blackColor(), [833, 350, 7, 7])
+    pygame.draw.ellipse(screen, PYColor.blackColor(), [843, 342, 7, 7])
 
-    pygame.draw.line(screen, PYColor.darkBownColor(), [795, 400], [750, 330], 10)
-    pygame.draw.line(screen, PYColor.darkBownColor(), [853, 390], [897, 331], 10)
+    if arms_up:
+        pygame.draw.line(screen, PYColor.darkBownColor(), [795, 400], [750, 330], 10)
+        pygame.draw.line(screen, PYColor.darkBownColor(), [853, 390], [897, 331], 10)
+    else:
+        pygame.draw.line(screen, PYColor.darkBownColor(), [811, 372], [743, 424], 10)
+        pygame.draw.line(screen, PYColor.darkBownColor(), [823, 375], [907, 421], 10)
 
     pygame.draw.ellipse(screen, PYColor.whiteColor(), [780, 360, 90, 90])
     pygame.draw.ellipse(screen, PYColor.blackColor(), [780, 360, 90, 90], 1)
